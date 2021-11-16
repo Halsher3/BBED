d597490063fba105ce9df3b207c29c6b77a28a66
Big B <bradeno28@gmail.com>
2021-11-16 16:17:52 -0500

    Add files via upload

---
+++ Astromath/src/Practice.java
@@ -0,0 +1,37 @@
+public class Practice {
+	private static int courseSelection;
+	private static string Questions;
+	// comment scetion
+
+	//constructor
+	public Practice(int courseSelection, String questions,//commentsection) {
+		this.courseSelection = courseSelection;
+		this.questions = questions
+		this.comments = comments
+	}
+
+	//defines grades? 
+
+	// calls Question class
+	String getQuestions (string Question) {
+		return this.Question;
+	}
+
+	//checks the answer for correctness
+	public static boolean isCorrect(int userInput) {
+		boolean isTrue = false;
+		
+		if(userInput == answer) {
+			isTrue = true;
+		}
+		
+		return isTrue;
+	}
+
+	//sets grade for practice test
+	void setGrade(String grades) {
+		this.grades = grades
+	
+
+
+		 
