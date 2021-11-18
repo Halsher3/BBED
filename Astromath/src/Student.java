619105cc36166360a294f7ec2883692a4f7d8bb4
Big B <bradeno28@gmail.com>
2021-11-18 16:36:41 -0500

    Add files via upload

---
+++ Astromath/src/Student.java
@@ -0,0 +1,62 @@
+public class Student{
+	private static String username;
+	private static String email;
+	private static String passWord;
+	private static int accNum;
+	private static int gradeLevel;
+	private static int level;
+	private static int testScores;
+
+	//constructor
+	public Student(String username, String email, String passWord, int accNum, int gradeLevel, int level, int testScores ) {
+		this.username = username;
+		this.email = email;
+		this.passWord = passWord;
+		this.accNum = accNum;
+		this.gradeLevel  gradeLevel;
+		this.level = level;
+		this.testScores = testScores; 
+	} 
+
+	public void setUsername(String username){
+		username = new uname;
+	}
+	public void setPassword(String passWord){
+		passWord = new pword;
+	}
+	public void setEmail(String email){
+		email = new mail;
+	}
+
+	//construtor for login info
+
+
+	void getPassword (String passWord){
+		return this.passWord;
+	}
+
+
+	void getLogin (String login) {
+
+		if (username.equals(passWord))
+
+		return this.login;
+	}
+
+	void getAccNum (int accNum) {
+		return this.accNum;
+	}
+
+	void getGrade (int gradeLevel){
+		return this.gradeLevel;
+	}
+
+	void getLevel (int level) {
+		return this.level;
+	}
+
+}
+
+
+
+		 
