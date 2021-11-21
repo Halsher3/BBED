7cfc2df668dcf3f85b9385165202f85bff23c04c
pluralslash <90721914+pluralslash@users.noreply.github.com>
2021-11-20 21:13:34 -0500

    Update Visual.java

--- Astromath/src/Visual.java
+++ Astromath/src/Visual.java
@@ -1,16 +1,19 @@
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JTextField;
+import javax.swing.*;
 import java.awt.Color;
 import java.awt.Font;
-import javax.swing.JLabel;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Visual {
+public class Visual extends JFrame
+{
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField text_username;
+	private JPasswordField text_password;
+	private Student goku = new Student("goku7", "menameisgoku@gmail.com", "dragonballs", 1234, 1, 1);
 
 	/**
 	 * Launch the application.
@@ -64,22 +67,55 @@ public class Visual {
 		lblNewLabel_2.setBounds(310, 147, 745, 156);
 		frame.getContentPane().add(lblNewLabel_2);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
-		textField.setForeground(Color.WHITE);
-		textField.setBackground(new Color(26, 38, 83));
-		textField.setBounds(373, 357, 447, 81);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
+		text_username = new JTextField();
+		text_username.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
+		text_username.setForeground(Color.WHITE);
+		text_username.setBackground(new Color(26, 38, 83));
+		text_username.setBounds(373, 357, 447, 81);
+		frame.getContentPane().add(text_username);
+		text_username.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setForeground(Color.WHITE);
-		textField_1.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
-		textField_1.setColumns(10);
-		textField_1.setBackground(new Color(26, 38, 83));
-		textField_1.setBounds(373, 496, 447, 81);
-		frame.getContentPane().add(textField_1);
-
+		text_password = new JPasswordField();
+		text_password.setForeground(Color.WHITE);
+		text_password.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
+		text_password.setColumns(10);
+		text_password.setBackground(new Color(26, 38, 83));
+		text_password.setBounds(373, 496, 447, 81);
+		frame.getContentPane().add(text_password);
+		
+		JLabel label_incorrect = new JLabel("Incorrect username or password!");
+		label_incorrect.setVisible(false);
+		label_incorrect.setForeground(new Color(255, 66, 66));
+		label_incorrect.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		label_incorrect.setBounds(373, 276, 487, 26);
+		frame.getContentPane().add(label_incorrect);
+		
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) 
+			{
+				if(goku.getLogin(text_username.getText(), String.valueOf(text_password.getPassword())))
+				{
+					MainWindow home = new MainWindow();
+					frame.dispose();
+				}
+				else
+				{
+					
+					label_incorrect.setVisible(true);
+				}
+			}
+		});
+		btnLogin.setForeground(Color.BLACK);
+		btnLogin.setFont(new Font("A-Space Demo", Font.PLAIN, 24));
+		btnLogin.setBounds(412, 588, 370, 68);
+		frame.getContentPane().add(btnLogin);
+		
+		frame.setVisible(true);
+		
+		
 		
 	}
 }
