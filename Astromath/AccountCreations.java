ca5226d267a04f062a8d43af83f27646410876fd
Halsher3 <57731203+Halsher3@users.noreply.github.com>
2021-12-12 16:12:38 -0500

    database is live
    //DATABASE CONNECTION LINE IS IN DISCORD is the line where the connection line should be

---
+++ Astromath/AccountCreations.java
@@ -0,0 +1,237 @@
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import java.util.Random;
+
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+//sql stuff
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class AccountCreations {
+
+	private JFrame frame;
+	private JTextField gradeLevel;
+	private JTextField GovName;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AccountCreations window = new AccountCreations();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public AccountCreations() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.getContentPane().setForeground(Color.WHITE);
+		frame.getContentPane().setBackground(new Color(77,58,129));
+		frame.setBounds(100, 100, 1278, 720);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+	
+		JLabel userLabel = new JLabel("Create a Username");
+		userLabel.setForeground(Color.WHITE);
+		userLabel.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		userLabel.setBounds(52, 101, 447, 81);
+		frame.getContentPane().add(userLabel);
+		
+		JLabel emailLabel = new JLabel("Add an Email Address");
+		emailLabel.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		emailLabel.setForeground(Color.WHITE);
+		emailLabel.setBounds(52, 206, 447, 68);
+		frame.getContentPane().add(emailLabel);
+		
+		
+		JLabel passwordLabel = new JLabel("Enter a Password");
+		passwordLabel.setForeground(Color.WHITE);
+		passwordLabel.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		passwordLabel.setBounds(52, 304, 447, 68);
+		frame.getContentPane().add(passwordLabel);
+		
+		JLabel gradeLabel = new JLabel("Enter your grade level");
+		gradeLabel.setForeground(Color.WHITE);
+		gradeLabel.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		gradeLabel.setBounds(413, 625, 346, 39);
+		frame.getContentPane().add(gradeLabel);
+
+		JLabel astromath = new JLabel("ASTROMATH");
+		astromath.setForeground(Color.WHITE);
+		astromath.setFont(new Font("a Atmospheric", Font.PLAIN, 36));
+		astromath.setBounds(98, 39, 745, 52);
+		frame.getContentPane().add(astromath);
+		
+		JTextField userName = new JTextField();
+		userName.setFont(new Font("A-Space Demo", Font.PLAIN, 26));
+		userName.setForeground(Color.WHITE);
+		userName.setBackground(new Color(26, 38, 83));
+		userName.setBounds(52, 163, 592, 56);
+		frame.getContentPane().add(userName);
+		userName.setColumns(10);
+		
+		JTextField userEmail = new JTextField();
+		userEmail.setForeground(Color.WHITE);
+		userEmail.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
+		userEmail.setColumns(10);
+		userEmail.setBackground(new Color(26, 38, 83));
+		userEmail.setBounds(52, 259, 592, 56);
+		frame.getContentPane().add(userEmail);
+		
+		JTextField userPassword = new JTextField();
+		userPassword.setForeground(Color.WHITE);
+		userPassword.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
+		userPassword.setColumns(10);
+		userPassword.setBackground(new Color(26, 38, 83));
+		userPassword.setBounds(52, 352, 592, 56);
+		frame.getContentPane().add(userPassword);
+		
+		gradeLevel = new JTextField();
+		gradeLevel.setForeground(Color.WHITE);
+		gradeLevel.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		gradeLevel.setBackground(new Color(26, 38, 83));
+		gradeLevel.setBounds(769, 596, 190, 68);
+		frame.getContentPane().add(gradeLevel);
+		gradeLevel.setColumns(10);
+		
+		GovName = new JTextField();
+		GovName.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		GovName.setBackground(new Color(26, 38, 83));
+		GovName.setBounds(52, 533, 592, 56);
+		frame.getContentPane().add(GovName);
+		GovName.setColumns(10);
+		
+		JLabel casePasswordReq = new JLabel("Your password must have at least 1 Uppercase character\r\n");
+		casePasswordReq.setForeground(Color.WHITE);
+		casePasswordReq.setFont(new Font("A-Space Demo", Font.PLAIN, 15));
+		casePasswordReq.setBounds(52, 399, 604, 52);
+		frame.getContentPane().add(casePasswordReq);
+		
+		JLabel characterReq = new JLabel("At least 6 characters long\r\n");
+		characterReq.setForeground(Color.WHITE);
+		characterReq.setFont(new Font("A-Space Demo", Font.PLAIN, 15));
+		characterReq.setBounds(52, 432, 604, 39);
+		frame.getContentPane().add(characterReq);
+		
+		JLabel specialCharReq = new JLabel("At least one special character (!, ?, /, *, &, ^, etc.)\r\n");
+		specialCharReq.setForeground(Color.WHITE);
+		specialCharReq.setFont(new Font("A-Space Demo", Font.PLAIN, 15));
+		specialCharReq.setBounds(52, 461, 604, 39);
+		frame.getContentPane().add(specialCharReq);
+		
+		JLabel home = new JLabel("");
+		home.setIcon(new ImageIcon("C:\\Users\\halsh\\Downloads\\home.png"));
+		home.setBounds(30, 23, 78, 81);
+		frame.getContentPane().add(home);
+		
+
+		
+		
+		
+		//my sql database register stuff
+		JButton registerButton = new JButton("Register");
+		registerButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				var username = userName.getText();
+				var email = userEmail.getText();
+				var password = userEmail.getText();
+				var grade = gradeLevel.getText();
+				var name = GovName.getText();
+				
+				//userID
+				int userID;
+				Random rand = new Random();
+				userID = rand.nextInt(1000000 - 100000) + 100000;
+				
+				try {
+                    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3307/userregistration","root","ubuntu123");
+                    
+                    String query  = "SELECT * from userinfo WHERE userID = '" + userID + "'";
+                    Statement st = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+                    ResultSet rs = st.executeQuery(query); // execute the query, and get a java resultset
+
+                    // if this ID already exists, we quit
+                    if(rs.absolute(1)) {
+                    	connection.close();
+                         return;
+                    }
+
+                    String query1 = "INSERT INTO userinfo(`username`, `email`, `password`, `grade`, `name`, userID) "
+                    		+ "VALUES ('" + username + "','" + password + "','" + email + "','" + grade +"','" + name + "','" + userID + "')";
+                                                    
+                    Statement sta = connection.createStatement();
+                    int x = sta.executeUpdate(query1);
+                    if (x == 0) {
+                        JOptionPane.showMessageDialog(registerButton, "This is alredy exist");
+                    } else {
+                        String msg = null;
+						JOptionPane.showMessageDialog(registerButton,
+                            "Welcome, " + msg + "Your account is sucessfully created");
+                    }
+                    connection.close();
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });	
+		registerButton.setFont(new Font("A-Space Demo", Font.PLAIN, 27));
+		registerButton.setBounds(1009, 594, 218, 68);
+		frame.getContentPane().add(registerButton);
+		
+		JLabel astronaut = new JLabel("");
+		astronaut.setIcon(new ImageIcon("C:\\Users\\halsh\\Downloads\\astronaut.png"));
+		astronaut.setBounds(614, -93, 1009, 960);
+		frame.getContentPane().add(astronaut);
+		
+		JLabel LabelName = new JLabel("Enter a display name");
+		LabelName.setForeground(new Color(255, 255, 255));
+		LabelName.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		LabelName.setBounds(52, 495, 766, 32);
+		frame.getContentPane().add(LabelName);
+		
+
+		
+
+		
+
+		
+		//end of button function to create a user
+		
+		
+		
+	
+
+		
+	}
+}
---
+++ Astromath/Login.java
@@ -0,0 +1,197 @@
+import java.awt.EventQueue;
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.*;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Login extends JPanel 
+{
+
+	private JPanel panel_login = new JPanel();
+	private JTextField textUsername;
+	private JPasswordField textPassword;
+	private Student goku = new Student("goku7", "menameisgoku@gmail.com", "dragonballs", 1234, 1, 1);
+	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private int height = screenSize.height;
+	private int width = screenSize.width;
+	
+
+	
+
+	/**
+	 *
+	 * public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Visual window = new Visual();
+					window.panel_login.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	 *
+	 *
+	 */
+	
+
+	/**
+	 * Create the application.
+	 */
+	public Login(JLayeredPane lp, Test test)
+	{
+		
+		panel_login.setBackground(new Color(77,58,129));
+		panel_login.setBounds(0, 0, 1262, 681);
+		lp.add(panel_login);
+		panel_login.setLayout(null);
+		
+		JLabel labelUsername = new JLabel("Username");
+		labelUsername.setForeground(Color.WHITE);
+		labelUsername.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		labelUsername.setBounds(373, 292, 337, 81);
+		panel_login.add(labelUsername);
+		
+		JLabel labelPassword = new JLabel("Password");
+		labelPassword.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		labelPassword.setForeground(Color.WHITE);
+		labelPassword.setBounds(373, 430, 220, 68);
+		panel_login.add(labelPassword);
+		
+		JLabel astromath = new JLabel("ASTROMATH");
+		astromath.setForeground(Color.WHITE);
+		astromath.setFont(new Font("a Atmospheric", Font.PLAIN, 84));
+		astromath.setBounds(261, 146, 745, 156);
+		panel_login.add(astromath);
+		
+		textUsername = new JTextField();
+		textUsername.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
+		textUsername.setForeground(Color.WHITE);
+		textUsername.setBackground(new Color(26, 38, 83));
+		textUsername.setBounds(373, 357, 447, 81);
+		textUsername.setBorder(new LineBorder(new Color(0, 195, 255), 5, true));
+		panel_login.add(textUsername);
+		textUsername.setColumns(10);
+		
+		textPassword = new JPasswordField();
+		textPassword.setForeground(Color.WHITE);
+		textPassword.setFont(new Font("A-Space Demo", Font.PLAIN, 42));
+		textPassword.setColumns(10);
+		textPassword.setBackground(new Color(26, 38, 83));
+		textPassword.setBounds(373, 496, 447, 81);
+		textPassword.setBorder(new LineBorder(new Color(0, 195, 255), 5, true));
+		panel_login.add(textPassword);
+		
+		JLabel labelIncorrect = new JLabel("Incorrect username or password!");
+		labelIncorrect.setVisible(false);
+		labelIncorrect.setForeground(new Color(255, 66, 66));
+		labelIncorrect.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		labelIncorrect.setBounds(373, 276, 487, 26);
+		panel_login.add(labelIncorrect);
+		
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try  {
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3307/userregistration","root","ubuntu123");
+					Statement stmt=con.createStatement();
+					var sql="Select * from userinfo where username='"+textUsername.getText()+"' and password='"+textPassword.getText()+"'";
+					ResultSet rs=stmt.executeQuery(sql);
+					if(rs.next())
+						JOptionPane.showMessageDialog(null,"Login Succ"); 
+					else
+						labelIncorrect.setVisible(true);
+					con.close();
+					
+					/*
+					 The login will connect and work but it needs to lead into the homepage with all its data
+					 */
+						
+				}catch(Exception w) {System.out.print(w);}
+			}
+		});
+		
+		/* OLD LOGIN STUFF
+		btnLogin.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) 
+			{
+				if(goku.getLogin(textUsername.getText(), String.valueOf(textPassword.getPassword())))
+				{
+					MainWindow panel_home = new MainWindow(lp);
+					switch_screen(panel_home.getPanel(), lp);
+				}
+				else
+				{
+					
+					labelIncorrect.setVisible(true);;
+				}
+			}
+		});
+		*/
+		
+		btnLogin.setForeground(Color.BLACK);
+		btnLogin.setFont(new Font("A-Space Demo", Font.PLAIN, 24));
+		btnLogin.setBounds(412, 588, 370, 68);
+		panel_login.add(btnLogin);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(".\\\\assets\\\\images\\\\astro.png"));
+		lblNewLabel.setBounds(-19, 325, 370, 424);
+		panel_login.add(lblNewLabel);
+		
+		JButton lblNewLabel_1 = new JButton("New Student?");
+		lblNewLabel_1.setBackground(new Color(26, 38, 83));
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("A-Space Demo", Font.PLAIN, 22));
+		lblNewLabel_1.setForeground(new Color(127, 255, 212));
+		lblNewLabel_1.setBounds(964, 602, 288, 68);
+		panel_login.add(lblNewLabel_1);
+	}
+
+	/**
+	 * Initialize the contents of the panel_login.
+	 */
+	//private void initialize() {
+
+		
+		
+	
+		
+		
+		
+	//}
+	
+	public JPanel getPanel()
+	{
+		return panel_login;
+		
+	}
+	public void switch_screen(JPanel p, JLayeredPane lp)
+	{
+		lp.removeAll();
+		p.setLayout(null);
+		lp.add(p);
+		lp.repaint();
+		lp.revalidate();
+		
+
+	}
+	
+}
